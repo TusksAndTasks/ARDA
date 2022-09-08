5575c607e6a2939db2c308e9e13f0bd1d0828956
TusksAndTasks <thedestroyerofworlds@mail.ru>
2022-09-08 18:11:49 +0300

    feat: add lead-exchange page

--- src/components/PersonalCabinet/LeadExchange.tsx
+++ src/components/PersonalCabinet/LeadExchange.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import TypographyPrimitive, { TypographyModes } from '../../primitives/TypographyPrimitive';
+import LeadExchangeData from '../../data/CabinetLeadExchange/LeadExchange.json';
+import LinkPrimitive from '../../primitives/LinkPrimitive';
 
 function LeadExchange() {
-  return <div>Exchange</div>;
+  return (
+    <div className="ml-3">
+      <TypographyPrimitive as="h2" mode={TypographyModes.TITULAR}>
+        {LeadExchangeData.title}
+      </TypographyPrimitive>
+      <TypographyPrimitive as="p">{LeadExchangeData.communityRules}</TypographyPrimitive>
+      <LinkPrimitive href={LeadExchangeData.chatLink}>{LeadExchangeData.chatLink}</LinkPrimitive>
+      <div className="h-[200px] w-[200px] bg-black"></div>
+      <div>
+        <TypographyPrimitive as="h2" mode={TypographyModes.TITULAR}>
+          {LeadExchangeData.linksSection.title}
+        </TypographyPrimitive>
+        {LeadExchangeData.linksSection.links.map((linksBlock) => (
+          <div key={linksBlock.theme}>
+            <TypographyPrimitive as="p">{linksBlock.theme}</TypographyPrimitive>
+            {linksBlock.thematicLinks.map((link) => (
+              <LinkPrimitive key={link} href={link}>
+                {link}
+              </LinkPrimitive>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default React.memo(LeadExchange);
